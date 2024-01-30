df2804c067123deff6062598552948501c03812f
Guilherme Pacheco <guilherme-p-medeiros@outlook.com>
2024-01-30 12:45:32 -0300

    Update files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { ButtonLinkComponent } from './components/button-link/button-link.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    ButtonLinkComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
